88f9cb937c5e6e3eeaaf56c5b59ff06393da810b
sin00528 <sin00528@gmail.com>
2020-06-06 14:50:36 +0900

    added picture upload component

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageUploader from "react-images-upload";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(pictureFiles, pictureDataURLs) {
+    this.setState({
+      pictures: this.state.pictures.concat(pictureFiles)
+    });
+    // 여기에 http 요청 작성
+  }
+
+  render() {
+    return (
+      <ImageUploader
+        withIcon={true}
+        buttonText="이미지를 선택하세요"
+        onChange={this.onDrop}
+        imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+        maxFileSize={5242880}
+        withPreview={true}
+      />
+    );
+  }
 }
 
 export default App;
